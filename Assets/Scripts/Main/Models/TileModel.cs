2e9dd25217c9e42e9f59b7ba13e223e0714eb657
Sudiksha <sudiksha.sd1@gmail.com>
2020-06-11 03:56:12 +0530

    Added game files

---
+++ Assets/Scripts/Main/Models/TileModel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ItemFinder.Models
+{
+    public class TileModel : MonoBehaviour
+    {
+        #region --------------------- Model Properties --------------------- 
+        public int currentPositionId;
+        public bool isItemShown = false;
+        #endregion
+    }
+}
